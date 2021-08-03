a9ca0c80b6a6adaf3e0dac28beedfc02390498d2
Ozzy <Ozols.ugiss@gmail.com>
2021-08-03 21:47:12 +0300

    http -> https

--- app/src/main/java/com/ozolsugis/weatherapp/util/Constants.kt
+++ app/src/main/java/com/ozolsugis/weatherapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.ozolsugis.weatherapp.util
 
 object Constants {
 
-    const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
+    const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
     const val LOCATION_PERMISSION_REQUEST_CODE = 0
     const val LOCATION_REQUEST_INTERVAL : Long = 0
     const val LOCATION_REQUEST_FASTEST_INTERVAL : Long = 0
