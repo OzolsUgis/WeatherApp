98e48c21e2f342c4b847a288ddc6e66a832f1982
Ozzy <Ozols.ugiss@gmail.com>
2021-08-03 21:46:18 +0300

    Test ui started & delay for location update simulated

--- app/src/main/java/com/ozolsugis/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/ozolsugis/weatherapp/presentation/MainActivity.kt
@@ -8,19 +8,40 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.ozolsugis.weatherapp.presentation.homepage.WeatherAppHomePage
 import com.ozolsugis.weatherapp.presentation.ui.theme.WeatherAppTheme
+import com.ozolsugis.weatherapp.services.LocationService
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            WeatherAppTheme {
+        val locationUpdate = LocationService(this,this)
+        locationUpdate.getLocationUpdate()
 
+
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(2000L)
+            var latitude: Double = locationUpdate.latitude
+            var longitude: Double = locationUpdate.longitude
+
+            setContent {
+                WeatherAppTheme {
+                    WeatherAppHomePage(
+                        latitude = latitude,
+                        longitude = longitude
+                    )
+                }
             }
         }
+
     }
 }
 
