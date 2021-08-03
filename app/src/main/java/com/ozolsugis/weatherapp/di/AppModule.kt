ab43b2a2987cbacc238f6a40c5827ee83f75e032
Ugis <ozols.ugiss@gmail.com>
2021-08-03 18:07:55 +0300

    Revert "Weather repository & weatherApi dependencies provided"
    This reverts commit b19fdb08

--- app/src/main/java/com/ozolsugis/weatherapp/di/AppModule.kt
+++ app/src/main/java/com/ozolsugis/weatherapp/di/AppModule.kt
@@ -1,30 +1,10 @@
 package com.ozolsugis.weatherapp.di
 
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun provideWeatherRepository(
-        api : WeatherApi
-    ) = WeatherRepository(api)
-
-    @Singleton
-    @Provides
-    fun provideWeatherApi(): WeatherApi {
-        return Retrofit.Builder()
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-            .create(WeatherApi::class.java)
-    }
 }
