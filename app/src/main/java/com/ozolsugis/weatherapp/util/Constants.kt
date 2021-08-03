b851c595261bfa4913c9387c65a5aa4aeec9f0c5
Ugis <ozols.ugiss@gmail.com>
2021-08-03 18:05:48 +0300

    constants for Location Request specified

---
+++ app/src/main/java/com/ozolsugis/weatherapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.ozolsugis.weatherapp.util
+
+object Constants {
+    const val LOCATION_PERMISSION_REQUEST_CODE = 0
+    const val LOCATION_REQUEST_INTERVAL : Long = 0
+    const val LOCATION_REQUEST_FASTEST_INTERVAL : Long = 0
+    const val LOCATION_REQUEST_NUM_UPDATES = 1
+}
