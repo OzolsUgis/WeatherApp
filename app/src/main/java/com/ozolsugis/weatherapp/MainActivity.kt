47fcade2e7cb74c00a15924cf8c4b7a3af83b645
Ugis <ozols.ugiss@gmail.com>
2021-08-03 14:32:38 +0300

    android entry point annotation added to main activity

--- app/src/main/java/com/ozolsugis/weatherapp/MainActivity.kt
+++ app/src/main/java/com/ozolsugis/weatherapp/MainActivity.kt
@@ -9,7 +9,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.ozolsugis.weatherapp.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
