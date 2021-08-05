f74fb5b1ee7748d498b50c2c9b66411eb44f92f3
Ozzy <Ozols.ugiss@gmail.com>
2021-08-05 19:07:46 +0300

    Colors for background specified

--- app/src/main/java/com/ozolsugis/weatherapp/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/ozolsugis/weatherapp/presentation/ui/theme/Color.kt
@@ -2,13 +2,33 @@ package com.ozolsugis.weatherapp.presentation.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
 
 val LightOrange = Color(0xFFEE9B00)
-val RainyBlue = Color (0xFF005F73)
-val CleanBlue = Color (0xFF94D2BD)
+
+val CleanBlue = Color(0xFF568ED1)
 val TextColor = Color (0xFFE9D8A6)
 val NightBlue = Color(0xFF6930c3)
+
+
+val LightGray = Color(0xFFadb5bd)
+val MediumGray = Color(0xFF343a40)
+val MediumBlue = Color(0xFF356AAA)
+
+val ThunderBlueDay = Color(0xFF45658D)
+val ThunderGrayDay = Color(0xFF103B6F)
+val SnowWhite = Color(0xFFCEF5F7)
+val SnowWhiteNight = Color(0xFF95A8B3)
+
+val DayBlue = Color(0xFF9BE8FF)
+val DayMediumBlue = Color(0xFF4cc9f0)
+val DaySun = Color(0xFFFFF200)
+
+val NightDark = Color(0xFF00296b)
+val NightMedium = Color(0xFF0D5AA5)
+val NightMediumMid = Color(0xFF526475)
+
+
+val CloudsDayLight = Color(0xFFE7E3FF)
+val CloudsDayMedium = Color(0xFFC7C3DF)
+val CloudsNightDark = Color(0xFF696677)
+val CloudsNightLight = Color(0xFFA8A4BA)
